f97ebeded5efc23a5918b7cc2c2cd26dbc2c6bbc
FairTex <makarov906@gmail.com>
2016-12-02 12:30:29 +0500

    Init

---
+++ CodeFirst/Model.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Device.Location;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    public class Base
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class Point
+    {
+        [Key]
+        public int PointId { get; set; }
+
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public GeoCoordinate Coordinate { get; set; }
+
+        [Range(0, 5)]
+        public int Raiting { get; set; }
+        public virtual List<Review> Reviews { get; set; }
+    }
+
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+
+        public string Text { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ CodeFirst/SampleContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirst
+{
+    public class DatabaseContext : DbContext
+    {
+        // Имя будущей базы данных можно указать через
+        // вызов конструктора базового класса
+        public DatabaseContext() : base("TravelProject")
+        { }
+
+        // Отражение таблиц базы данных на свойства с типом DbSet
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Point> Points { get; set; }
+        public DbSet<Base> Base { get; set; }
+    }
+}
